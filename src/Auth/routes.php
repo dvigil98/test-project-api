8e92637851e4ce5f8197d333f65e693592f05b75
David Vigil <vigilgarcia98@gmail.com>
2024-03-24 19:11:04 -0700

    Rutas de prueba removidas del modulo de autenticacion

--- src/Auth/routes.php
+++ src/Auth/routes.php
@@ -17,15 +17,7 @@ use Src\Auth\Http\Controllers\AuthController;
 // login
 Route::post('/api/auth/login', [AuthController::class, 'login']);
 
-Route::get('/api/auth/out', function () {
-    return response()->json(['message' => 'Out - Ready'], 200);
-});
-
 Route::middleware(['auth:api'])->group(function () {
-
-    Route::get('/api/auth/in', function () {
-        return response()->json(['message' => 'In - Ready'], 200);
-    });
 
     // logout
     Route::get('/api/auth/logout', [AuthController::class, 'logout']);
